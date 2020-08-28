2d64ebb458b07215d5f40fab75c67a8e65336482
joe <47951551+joeromsa@users.noreply.github.com>
2020-08-28 10:21:08 -0500

    Added basic components and routing.

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import {BrowserRouter as Router, Switch, Route, Link, useParams, useHistory} from 'react-router-dom'
+
+//components
+import About from './components/About'
+import Menu from './components/Menu'
+import Search from './components/Search'
+
+const App = () => {
+    return (
+        <div>
+            <h1>Drink App</h1>
+            <Router>
+                <Menu />
+                <Switch>
+                    <Route path="/about">
+                        <About />
+                    </Route>
+                    <Route path="/">
+                        <Search />
+                    </Route>
+                </Switch>
+            </Router>
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/About.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h2>About Drink App</h2>
+            <p>Drink App is dedicated to good drinks and good times with family and friends. Our goal is give 
+                anyone the opportunity to make their favorite drinks from the comfort of their home. We also think
+                doing things your own way is import too, so we also have provided the opportunity to create your own 
+                original recipes, and share them with the world. 
+            </p>
+            <h2>Meet the team</h2>
+            <h3>Joe Romsa</h3>
+            <h4>Creator of Drink App</h4>
+            <p>Joe has spent the last few years after turning twenty one exploring the world of cocktails. From this 
+                he found a passion for making his own drinks. He took this and combined it with his other passion for computer
+                science in order to create this site to help others discover a love for cocktail making.
+                <br/>
+                Favorite bar: Laka Lono Rum Club, Omaha<br/>
+                Favorite drink: Mai Tai
+            </p>
+        </div>
+    )
+}
+
+export default About
